496d283943495167696b9c82f72ab2cd355da41c
ELLOWE <47371493+ELLowe@users.noreply.github.com>
2019-09-21 12:27:22 -0500

    hosting the data for united project

---
+++ united_stuff/static/js/config.js
@@ -0,0 +1,16 @@
+// API key
+var API_KEY = "pk.eyJ1IjoiZWxsb3dlIiwiYSI6ImNrMHRyejFnMzBkdjIzY3JyN2R1ZjQ4dW8ifQ.elgKcrUWPzminE_EPmgCYA";
+
+const attribution = `
+  Map data & copy;
+  <a href="https://www.openstreetmap.org/">
+    OpenStreetMap
+    </a>
+  contributors,
+    <a href="https://creativecommons.org/licenses/by-sa/2.0/">
+      CC-BY-SA
+    </a>
+    , Imagery ©
+  <a href="https://www.mapbox.com/">
+    Mapbox
+    </a>`;
